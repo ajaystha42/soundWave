9c64062c611bcc0db17d0460dfe78cbbe3038905
nirajankarki5 <nirajankarki5@gmail.com>
2023-07-10 09:24:36 -0400

    added trending products

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 import products from "./data.js";
 
 window.onload = (e) => {
-  console.log("HELLOOOOO");
-
   // create a list of <li> and send to <ul>
   const createCardList = (className, category) => {
-    const headphoneList = document.querySelector(className);
+    const list = document.querySelector(className);
     products
       .filter((item) => item.category === category)
       .forEach((item) => {
@@ -14,10 +12,30 @@ window.onload = (e) => {
     <p>$${item.price}</p>
     <p>${item.availableQuantity}</p>`;
 
-        headphoneList.appendChild(li);
+        list.appendChild(li);
       });
   };
 
+  // Generate trending products
+  const generateTrendingList = () => {
+    const trendingList = document.querySelector(".trending-list");
+
+    const trendingProducts = [...products];
+    // Shuffle the array
+    const shuffled = trendingProducts.sort(() => 0.5 - Math.random());
+    let selected = shuffled.slice(0, 4);
+
+    selected.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<p>${item.name}</p>
+    <p>$${item.price}</p>
+    <p>${item.availableQuantity}</p>`;
+
+      trendingList.appendChild(li);
+    });
+  };
+
+  generateTrendingList();
   createCardList(".headphone-list", "HEADPHONE");
   createCardList(".earphone-list", "EARPHONE");
   createCardList(".speaker-list", "SPEAKER");
