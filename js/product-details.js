14d2fc834174ca45d6e506fcfc537e4ab3a1e654
nirajankarki5 <nirajankarki5@gmail.com>
2023-07-11 22:58:41 -0400

    added static product details

--- js/product-details.js
+++ js/product-details.js
@@ -1,8 +1,14 @@
+import products from "./data.js";
+
 // $(document).ready(function () {
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const id = urlParams.get("id");
+const id = parseInt(urlParams.get("id"));
 
-console.log(id);
+// get the product based on id using array.find() method
+const product = products.find((product) => product.id === id);
+
+console.log(product.name);
+
 // });
