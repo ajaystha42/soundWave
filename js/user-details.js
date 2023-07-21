d6c156b22ab83d17fcdf3589180cc5e629482342
nirajankarki5 <nirajankarki5@gmail.com>
2023-07-21 14:28:37 -0400

    improvements

--- js/user-details.js
+++ js/user-details.js
@@ -6,12 +6,12 @@ window.onload = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   console.log(user);
 
-  $("firstname").value = user.firstName;
-  $("lastname").value = user.lastName;
+  $("firstname").value = user.first_name;
+  $("lastname").value = user.last_name;
   $("address").value = user.address;
   $("email").value = user.email;
   $("password").value = user.password;
-  // $("datepicker").value = user.dob;
+  $("datepicker").value = user.dob;
   $("contact").value = user.contact;
   $("gender").value = user.gender;
   $("country_selector").value = user.country;
