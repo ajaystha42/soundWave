d6c13b4bf668b295354f1d80ba60b291bda89599
Ajay Shrestha <er.ajaystha@gmail.com>
2023-07-24 14:53:14 -0400

    cart bug fix

--- js/common.js
+++ js/common.js
@@ -1,9 +1,14 @@
-import { cart } from "./cart.js";
-
 window.onload = (e) => {
+  // if (cart) {
+  //   cart.items.length != 0
+  //     ? (document.getElementById("cart-number").innerHTML = cart.items.length)
+  //     : document.getElementById("cart-number").remove();
+  // }
+  cart = JSON.parse(localStorage.getItem("cart"));
   if (cart) {
-    cart.items.length != 0
-      ? (document.getElementById("cart-number").innerHTML = cart.items.length)
-      : document.getElementById("cart-number").remove();
+    cart.items.length != 0 &&
+      (document.getElementById("cart-number").innerHTML = cart.items.length);
+  } else {
+    document.getElementById("cart-number").remove();
   }
 };
