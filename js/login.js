5aa31f0ab110ad2c6151ab3a844c7339fa559529
nirajankarki5 <nirajankarki5@gmail.com>
2023-07-24 13:40:47 -0400

    added login validation

--- js/login.js
+++ js/login.js
@@ -1,12 +1,25 @@
 function validateForm() {
+  const emailPattern =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailError = document.getElementById("email-error");
+  const passwordError = document.getElementById("password-error");
+
+  emailError?.classList.add("error-hidden");
+  passwordError?.classList.add("error-hidden");
+
   let result = true;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  const emailError = document.getElementById("email-error");
-  const passwordError = document.getElementById("password-error");
   if (email === "" || password === "") {
-    if (emailError) emailError?.classList.remove("error-hidden");
-    if (passwordError) passwordError?.classList.remove("error-hidden");
+    if (email === "") emailError?.classList.remove("error-hidden");
+    if (password === "") passwordError?.classList.remove("error-hidden");
+    result = false;
+  }
+
+  // Using REGEX
+  else if (!email.match(emailPattern)) {
+    document.getElementById("email-error").textContent = "Invalid email";
+    emailError?.classList.remove("error-hidden");
     result = false;
   }
   return result;
