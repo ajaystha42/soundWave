96da292c85a3aca11e1983b84a0bed8d9487413e
Ajay Shrestha <er.ajaystha@gmail.com>
2023-07-25 20:03:33 -0400

    update user

--- php/update.php
+++ php/update.php
@@ -16,45 +16,36 @@ if (!$con) {
 }
 
 // Signup Submission
-if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Getting Value from Form
       $firstName = mysqli_real_escape_string($con, $_POST["firstname"]);
       $lastName = mysqli_real_escape_string($con, $_POST["lastname"]);
-      $password = mysqli_real_escape_string($con, $_POST["password"]);
-      $email = mysqli_real_escape_string($con, $_POST["email"]);
-      $gender = mysqli_real_escape_string($con, $_POST["gender"]);
-      $contact = mysqli_real_escape_string($con, $_POST["contact"]);
-      $country = mysqli_real_escape_string($con, $_POST["country"]);
       $address = mysqli_real_escape_string($con, $_POST["address"]);
-
-    // Encrypting Password
-    $encrypted_password = password_hash($password, PASSWORD_BCRYPT);
-
-
+      $contact = mysqli_real_escape_string($con, $_POST["contact"]);
+      $email = mysqli_real_escape_string($con, $_POST["email"]);
+      
     // SQL Query
-    $query = "UPDATE USERS SET firstName = '$firstName', lastName = '$lastName', password ='$password' gender ='$gender', 
-    contact ='$contact',country ='$country', address= '$address' WHERE email ='$email'";
-
+    $query = "UPDATE USERS SET first_name = '$firstName', last_name = '$lastName',
+    contact ='$contact', address= '$address' WHERE email ='$email'";
+    
     // Executing SQL Query
-    if(mysqli_query($con, $query)) echo "Data updated successfully!!";
+    if(mysqli_query($con, $query)) {
+        $query = "SELECT * FROM USERS WHERE email = '$email'";
+
+    // Executing SQL and fetching user's count
+        $result = mysqli_query($con, $query);
+         $user = mysqli_fetch_assoc($result);
+         echo "<script>localStorage.removeItem('user');</script>";
+
+         echo "<script>localStorage.setItem('user', '" . json_encode($user) . "');</script>";
+
+        echo "<script>window.location ='./../html/userDetails.html'</script>";
+    }
     else echo "Error Occured : " . $query . "<br>" . mysqli_error($con);
 
    
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        // SQL Query
-    $query = "SELECT * FROM USERS";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        echo "<h1>Users</h1>";
-        while ($row = $result->fetch_assoc()) {
-            echo "First Name: " . $row['first_name'] . ",Last Name: " . $row['last_name'] . ", Email: " . $row['email'] . ", Gender: " . $row['gender'] .", Contact: " . $row['contact'] .", Country: " . $row['country'] .", Address: " . $row['address'] . "<br>";
-        }
-    } else {
-        echo "No users found.";
-    }
-}
 
 
  // Closing Database Connection
